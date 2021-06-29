c0d53cd68a6532822f14ddc9316463e5578db3c2
Jéssica Neri <jess.neri94@gmail.com>
2021-06-29 10:30:56 -0300

    Feat: js components created

---
+++ js/modules/scroll-animation.js
@@ -0,0 +1,22 @@
+export default function initAnimacaoScroll() {
+    const sections = document.querySelectorAll(".js-scroll");
+  
+    if (sections.length) {
+      const windowMetade = window.innerHeight * 0.7;
+  
+      function animaScroll() {
+        sections.forEach((section) => {
+          const sectionTop = section.getBoundingClientRect().top;
+          const isSectionVisible = sectionTop - windowMetade < 0;
+  
+          if (isSectionVisible) {
+            section.classList.add("active");
+          }
+        });
+      }
+  
+      animaScroll();
+  
+      window.addEventListener("scroll", animaScroll);
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,13 @@
+import initSmoothScroll from "./modules/smooth-scroll.js";
+import initAnimacaoScroll from "./modules/scroll-animation.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
+initTabNav();
+initAccordion();
+initSmoothScroll();
+initAnimacaoScroll();
+initModal();
+initDropdownMenu();
